1ae83f18da5beed6b645a6d77992c907ad38dbbd
Crowdbotics <team@crowdbotics.com>
2021-08-30 01:05:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "proud_feather_30186Identifier",
+  url: "https://proud_feather_30186Identifier.botics.co",
+  api: "https://proud_feather_30186Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
